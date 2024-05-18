56ce96ab5d14d8deee48f9633b9791da35e349fe
Josip Begic <josipbegic57@gmail.com>
2024-05-18 02:37:41 +0200

    Initialize project

---
+++ src/main/java/com/nuvolo/nuvoloapi/NuvoloApiApplication.java
@@ -0,0 +1,13 @@
+package com.nuvolo.nuvoloapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NuvoloApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NuvoloApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nuvolo/nuvoloapi/NuvoloApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nuvolo.nuvoloapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NuvoloApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
