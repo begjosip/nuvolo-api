0004bde30ba8115ca8ea3aa28eb9c51cc77dd791
begjosip <josipbegic57@gmail.com>
2024-06-06 17:04:00 +0200

    Fix test application properties and test context load

--- src/test/java/com/nuvolo/nuvoloapi/NuvoloApiApplicationTests.java
+++ src/test/java/com/nuvolo/nuvoloapi/NuvoloApiApplicationTests.java
@@ -1,15 +1,23 @@
 package com.nuvolo.nuvoloapi;
 
+import com.nuvolo.nuvoloapi.controller.CategoryController;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 @ActiveProfiles("test")
 class NuvoloApiApplicationTests {
 
+    @Autowired
+    private CategoryController categoryController;
+
     @Test
     void contextLoads() {
+        assertThat(categoryController).isNotNull();
     }
 
 }
