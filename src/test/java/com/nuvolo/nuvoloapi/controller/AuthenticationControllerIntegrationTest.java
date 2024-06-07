9d6d941e7f0b4f8cd9aaf19519d4db60df525872
begjosip <josipbegic57@gmail.com>
2024-06-07 16:40:02 +0200

    Add forgotten password reset integration test

--- src/test/java/com/nuvolo/nuvoloapi/controller/AuthenticationControllerIntegrationTest.java
+++ src/test/java/com/nuvolo/nuvoloapi/controller/AuthenticationControllerIntegrationTest.java
@@ -37,12 +37,19 @@ class AuthenticationControllerIntegrationTest {
 
     private static final String AUTH_VERIFY_URI = "/v1/auth/verify";
 
+    private static final String PASSWORD_RESET_URI = "/v1/auth/reset-password";
+
     private static final String VALID_PASSWORD = "nuvolo-test-password";
 
     private static final String VERIFICATION_UUID_TOKEN = "123e4567-e89b-12d3-a456-426614174000";
 
     private static final String VERIFICATION_UUID__VERIFIED_TOKEN = "e7e66e28-90a8-468b-ab33-a0bdf3002c49";
 
+    private static final String PASSWORD_RESET_TOKEN = "e7e44e28-90a8-468b-cc33-a0bdf4002c49";
+
+    private static final String PASSWORD_RESET_TOKEN_2 = "b1e44e28-90a8-458b-dd33-a0cdf4002c49";
+
+    private static final String PASSWORD_RESET_UTILISED_TOKEN = "7e199298-1bf1-4c12-8056-15a740914c79";
 
     @Autowired
     private MockMvc mvc;
@@ -118,6 +125,75 @@ class AuthenticationControllerIntegrationTest {
                 .andReturn();
 
         assertEquals(HtmlUtil.UNSUCCESSFUL_VERIFICATION_HTML, response.getResponse().getContentAsString());
+    }
+
+    @Test
+    void testPasswordReset_success() throws Exception {
+        UserRequestDto changePasswordRequest = UserRequestDto.builder()
+                .email("verified@mail.com")
+                .password(VALID_PASSWORD)
+                .confirmPassword(VALID_PASSWORD)
+                .token(PASSWORD_RESET_TOKEN)
+                .build();
+        mvc.perform(MockMvcRequestBuilders.post(PASSWORD_RESET_URI)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(asJsonString(changePasswordRequest)))
+                .andDo(print())
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    void testPasswordReset_resetRequestTokenUtilised() throws Exception {
+        UserRequestDto changePasswordRequest = UserRequestDto.builder()
+                .email("verified@mail.com")
+                .password(VALID_PASSWORD)
+                .confirmPassword(VALID_PASSWORD)
+                .token(PASSWORD_RESET_UTILISED_TOKEN)
+                .build();
+        mvc.perform(MockMvcRequestBuilders.post(PASSWORD_RESET_URI)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(asJsonString(changePasswordRequest)))
+                .andDo(print())
+                .andExpect(jsonPath("$.title").value("Password reset error!"))
+                .andExpect(jsonPath("$.detail").value("No valid password reset requests found in 5 days. Send new request!"))
+                .andExpect(jsonPath("$.instance").value(PASSWORD_RESET_URI))
+                .andExpect(status().isBadRequest());
+    }
+
+    @Test
+    void testPasswordReset_passwordsAreNotMatching() throws Exception {
+        UserRequestDto changePasswordRequest = UserRequestDto.builder()
+                .email("verified@mail.com")
+                .password(VALID_PASSWORD)
+                .confirmPassword(VALID_PASSWORD.concat("."))
+                .token(PASSWORD_RESET_TOKEN_2)
+                .build();
+        mvc.perform(MockMvcRequestBuilders.post(PASSWORD_RESET_URI)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(asJsonString(changePasswordRequest)))
+                .andDo(print())
+                .andExpect(jsonPath("$.title").value("Passwords are not matching!"))
+                .andExpect(jsonPath("$.detail").value("Passwords are not matching"))
+                .andExpect(jsonPath("$.instance").value(PASSWORD_RESET_URI))
+                .andExpect(status().isBadRequest());
+    }
+
+    @Test
+    void testPasswordReset_unsupportedMediaType() throws Exception {
+        UserRequestDto changePasswordRequest = UserRequestDto.builder()
+                .email("verified@mail.com")
+                .password(VALID_PASSWORD)
+                .confirmPassword(VALID_PASSWORD)
+                .token(PASSWORD_RESET_TOKEN)
+                .build();
+        mvc.perform(MockMvcRequestBuilders.post(PASSWORD_RESET_URI)
+                        .content(asJsonString(changePasswordRequest)))
+                .andDo(print())
+                .andExpect(jsonPath("$.title").value("Unsupported media type!"))
+                .andExpect(jsonPath("$.status").value(415))
+                .andExpect(jsonPath("$.detail").value("Content-Type 'application/octet-stream' is not supported"))
+                .andExpect(jsonPath("$.instance").value(PASSWORD_RESET_URI))
+                .andExpect(status().isUnsupportedMediaType());
     }
 
     private static String asJsonString(final Object object) throws JsonProcessingException {
--- src/test/resources/sql/Insert_test_users.sql
+++ src/test/resources/sql/Insert_test_users.sql
@@ -1,11 +1,14 @@
 INSERT INTO nuvolo_user (first_name, last_name, email, password)
-VALUES ('nuvolo_first_name', 'nuvolo_last_name', 'nuvolo@mail.com', '$2a$12$8lPdJ9riIWZyGTDRITnltuyfSfGeSadNR85Pg/xxRE1ub9t5U2y6S');
+VALUES ('nuvolo_first_name', 'nuvolo_last_name', 'nuvolo@mail.com',
+        '$2a$12$8lPdJ9riIWZyGTDRITnltuyfSfGeSadNR85Pg/xxRE1ub9t5U2y6S');
 
 INSERT INTO nuvolo_user (first_name, last_name, email, password, is_enabled)
-VALUES ('nuvolo_first_name', 'nuvolo_last_name', 'verified@mail.com', '$2a$12$8lPdJ9riIWZyGTDRITnltuyfSfGeSadNR85Pg/xxRE1ub9t5U2y6S', TRUE);
+VALUES ('nuvolo_first_name', 'nuvolo_last_name', 'verified@mail.com',
+        '$2a$12$8lPdJ9riIWZyGTDRITnltuyfSfGeSadNR85Pg/xxRE1ub9t5U2y6S', TRUE);
 
 INSERT INTO nuvolo_user (first_name, last_name, email, password, is_enabled)
-VALUES ('nuvolo_first_name', 'nuvolo_last_name', 'unverified@mail.com', '$2a$12$8lPdJ9riIWZyGTDRITnltuyfSfGeSadNR85Pg/xxRE1ub9t5U2y6S', FALSE);
+VALUES ('nuvolo_first_name', 'nuvolo_last_name', 'unverified@mail.com',
+        '$2a$12$8lPdJ9riIWZyGTDRITnltuyfSfGeSadNR85Pg/xxRE1ub9t5U2y6S', FALSE);
 
 INSERT INTO verification (token, user_id)
 VALUES ('123e4567-e89b-12d3-a456-426614174000', (SELECT id
@@ -21,3 +24,19 @@ INSERT INTO verification (token, user_id)
 VALUES ('123e4567-e89b-12d3-a456-426614174044', (SELECT id
                                                  FROM nuvolo_user
                                                  WHERE email = 'unverified@mail.com'));
+
+
+INSERT INTO forgotten_pass_reset (token, user_id)
+VALUES ('e7e44e28-90a8-468b-cc33-a0bdf4002c49', (SELECT id
+                                                 FROM nuvolo_user
+                                                 WHERE email = 'verified@mail.com'));
+
+INSERT INTO forgotten_pass_reset (token, utilised, user_id)
+VALUES ('7e199298-1bf1-4c12-8056-15a740914c79', TRUE, (SELECT id
+                                                       FROM nuvolo_user
+                                                       WHERE email = 'verified@mail.com'));
+
+INSERT INTO forgotten_pass_reset (token, user_id)
+VALUES ('b1e44e28-90a8-458b-dd33-a0cdf4002c49', (SELECT id
+                                                 FROM nuvolo_user
+                                                 WHERE email = 'verified@mail.com'));
